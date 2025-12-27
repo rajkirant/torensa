3806fa33e7ea0ea1bd4ac39af67e8d02369f2f42
Raj Kiran <raj@ecogif.co>
2025-12-27 15:29:11 +0000

    setting up local and fist python api

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "***REMOVED-DJANGO-SECRET-KEY***"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['torensa.pythonanywhere.com']
+ALLOWED_HOSTS = ['torensa.pythonanywhere.com','127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "api",
 ]
 
 MIDDLEWARE = [
--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
 
     # React frontend (catch-all)
     re_path(r"^.*$", TemplateView.as_view(template_name="index.html")),
