274314d73896f2e86040b4617967dbe929455530
Raj Kiran <raj@ecogif.co>
2025-12-28 22:01:47 +0000

    reload website test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to new HMR
+          Edit <code>src/App.jsx</code> and save to fff HMR
         </p>
       </div>
       <p className="read-the-docs">
