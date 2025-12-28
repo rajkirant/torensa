868adab447cc00ca8f1105423a26bfb8461816f6
Raj Kiran <raj@ecogif.co>
2025-12-28 14:16:11 +0000

    seting up email sender

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import hello
+from .views import hello, send_test_email
 
 urlpatterns = [
     path("hello/", hello),
+    path("send-email/", send_test_email),
 ]
--- backend/settings.py
+++ backend/settings.py
@@ -134,3 +134,14 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "deploy/dist", "assets"),
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "rajkiran047@gmail.com"
+EMAIL_HOST_PASSWORD = "pjxv ssso xrzw srgy"
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
