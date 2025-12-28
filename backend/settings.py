208b4a34a70adb61cb32330a62c8277c5a556aa0
Raj Kiran <raj@ecogif.co>
2025-12-28 14:39:04 +0000

    trying ssl configuration

--- backend/settings.py
+++ backend/settings.py
@@ -138,9 +138,9 @@ STATICFILES_DIRS = [
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = "rajkiran047@gmail.com"
 EMAIL_HOST_PASSWORD = "pjxv ssso xrzw srgy"
 
