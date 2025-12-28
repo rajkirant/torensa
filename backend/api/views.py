6127b530b0f198df1e10758a091cd91382ca5c50
Raj Kiran <raj@ecogif.co>
2025-12-28 14:16:11 +0000

    seting up email sender

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,16 @@
 from django.http import JsonResponse
+from django.core.mail import send_mail
+
 
 def hello(request):
     return JsonResponse({"message": "Hello World"})
+
+def send_test_email(request):
+    send_mail(
+        subject="Test email from Torensa",
+        message="Hello! This is a test email sent from Django.",
+        from_email=None,
+        recipient_list=["rajkiran047@gmail.com"],
+        fail_silently=False,
+    )
+    return JsonResponse({"status": "Email sent"})
