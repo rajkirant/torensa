940853957fa61d35153b02fe1f7efb5733706d13
Raj Kiran <raj@ecogif.co>
2025-12-28 21:43:25 +0000

    PA token added as secret and test UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to testssss HMR
+          Edit <code>src/App.jsx</code> and save to new HMR
         </p>
       </div>
       <p className="read-the-docs">
