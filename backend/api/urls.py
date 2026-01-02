8f5424a56f3a79b21088aed8b3456c87e42742a0
Raj Kiran <raj@ecogif.co>
2026-01-02 21:40:26 +0000

    Basic bulk email functionality

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import hello, logout_view, me, send_test_email, signup_view
-from .views import login_view
+from .views.auth_views import hello, logout_view,login_view, me, signup_view
+from .views.email_views import send_email
 
 urlpatterns = [
     path("hello/", hello),
-    path("send-email/", send_test_email),
+    path("send-email/", send_email),
     path("login/", login_view),
     path("me/", me),
     path("logout/", logout_view),
