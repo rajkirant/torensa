99d9d9723c85aac6bf82eca11f0edd8aae87e19e
Raj Kiran <raj@ecogif.co>
2026-01-10 20:28:26 +0000

    new api endpoint

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "***REMOVED-DJANGO-SECRET-KEY***"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['torensa.pythonanywhere.com','127.0.0.1', 'localhost','torensa.com','torensa-backend.onrender.com']
+ALLOWED_HOSTS = ['torensa.pythonanywhere.com','127.0.0.1', 'localhost','torensa.com','torensa-backend.onrender.com','api.torensa.com']
 
 
 # Application definition
