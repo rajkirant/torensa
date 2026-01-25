689c877bd6fbe4a6c4bf9579d23038368902f7d5
Raj Kiran <raj@ecogif.co>
2026-01-25 20:36:12 +0000

    optimizations suggested by google

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.ts (or .js)
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -8,6 +9,28 @@ export default defineConfig({
       "/api": {
         target: "http://127.0.0.1:8000",
         changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // everything in node_modules goes into vendor-ish chunks
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) {
+              return "react-vendor";
+            }
+            if (
+              id.includes("@mui/material") ||
+              id.includes("@mui/icons-material")
+            ) {
+              return "mui-vendor";
+            }
+            // fallback: other vendor libs
+            return "vendor";
+          }
+        },
       },
     },
   },
