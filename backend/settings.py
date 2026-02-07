6d9c4076c942a46acd27c52cfc3a54ef548d388f
Raj Kiran <raj@ecogif.co>
2026-02-07 12:40:30 +0000

    allow access xi5o41nmf3.execute-api.us-east-1.amazonaws.com

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "***REMOVED-DJANGO-SECRET-KEY***"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.torensa.pythonanywhere.com','torensa.pythonanywhere.com','127.0.0.1', 'localhost','torensa.com','torensa-backend.onrender.com','api.torensa.com']
+ALLOWED_HOSTS = ['xi5o41nmf3.execute-api.us-east-1.amazonaws.com','www.torensa.pythonanywhere.com','torensa.pythonanywhere.com','127.0.0.1', 'localhost','torensa.com','torensa-backend.onrender.com','api.torensa.com']
 
 CORS_ALLOW_CREDENTIALS = True
 
